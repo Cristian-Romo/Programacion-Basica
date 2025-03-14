a6b7687131dfeb4f4f9727be3f8c7ff9eea66cbb
Cristian-Romo <L241050126@pabellon.tecnm.mx>
2025-03-14 08:00:51 -0600

    Diccionario 3 modificado

--- Unidad3/Python/Diccionarios3.py
+++ Unidad3/Python/Diccionarios3.py
@@ -7,10 +7,15 @@ estudiantes = {
     "Oswaldo": {"edad": 18, "carrera": "Mecatronica"},
     "Roberto": {"edad": 18, "carrera": "Mecatronica"},
     "Adan": {"edad": 18, "carrera": "Mecatronica"},
-    "Sabayd": {"edad": 18, "carrera": "Mecatronica"},
+    "Sabayd": {"edad": 19, "carrera": "Mecatronica"},
     "Juan": {"edad": 18, "carrera": "Mecatronica"},
     "Arath": {"edad": 18, "carrera": "Mecatronica"}
 }
+
+# Imprimir el diccionario completo
+print("Diccionario de estudiantes:")
+for nombre, detalles in estudiantes.items():
+    print(f"{nombre}: {detalles}")
 
 profesores = {
     "Eduardo": {"Materia": "Programacion Basica", "Promedio": 7.9},
@@ -21,3 +26,8 @@ profesores = {
     "El ASMR": {"Materia": "Algebra lineal", "Promedio": 0.0},
     "Abuelo de manuel": {"Materia": "Abuelo de manuel", "Promedio": 8.0}
 }
+
+# Imprimir el diccionario completo
+print("Diccionario de profesores:")
+for nombre, detalles in profesores.items():
+    print(f"{nombre}: {detalles}")
