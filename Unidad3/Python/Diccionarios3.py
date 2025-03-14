04621e8f247fa2b37a6da4a5bd25d55b164ec5fe
Cristian-Romo <L241050126@pabellon.tecnm.mx>
2025-03-14 07:52:49 -0600

    Create Diccionarios3.py

---
+++ Unidad3/Python/Diccionarios3.py
@@ -0,0 +1,23 @@
+estudiantes = {
+    "Ontiveros": {"edad": 18, "carrera": "Mecatronica"},
+    "Victor": {"edad": 18, "carrera": "Mecatronica"},
+    "Lupe": {"edad": 18, "carrera": "Mecatronica"},
+    "Aron": {"edad": 18, "carrera": "Mecatronica"},
+    "Alexa": {"edad": 18, "carrera": "Mecatronica"},
+    "Oswaldo": {"edad": 18, "carrera": "Mecatronica"},
+    "Roberto": {"edad": 18, "carrera": "Mecatronica"},
+    "Adan": {"edad": 18, "carrera": "Mecatronica"},
+    "Sabayd": {"edad": 18, "carrera": "Mecatronica"},
+    "Juan": {"edad": 18, "carrera": "Mecatronica"},
+    "Arath": {"edad": 18, "carrera": "Mecatronica"}
+}
+
+profesores = {
+    "Eduardo": {"Materia": "Programacion Basica", "Promedio": 7.9},
+    "La risitas": {"Materia": "Calcuo integral", "Promedio": 10.0},
+    "La gallos": {"Materia": "Contabilidad", "Promedio": 10.0},
+    "Finish Class": {"Materia": "Ingles", "Promedio": 8.4},
+    "El presumido": {"Materia": "Ingenieria de los materiales", "Promedio": 9.1},
+    "El ASMR": {"Materia": "Algebra lineal", "Promedio": 0.0},
+    "Abuelo de manuel": {"Materia": "Abuelo de manuel", "Promedio": 8.0}
+}
