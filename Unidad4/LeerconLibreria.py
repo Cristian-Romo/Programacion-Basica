043bb0a8aa7d0533ca5c74c596a7119267a19e9c
Cristian-Romo <L241050126@pabellon.tecnm.mx>
2025-03-21 07:50:46 -0600

    Importe de librerias

---
+++ Unidad4/LeerconLibreria.py
@@ -0,0 +1,16 @@
+from Archivos import leer_diccionarios_de_csv
+# Nombre del archivo a leer y de la función a importar
+
+archivo = "datos.csv"
+# Nombre del archivo
+datos = [
+    {"Nombre": "Juan", "Edad": 25, "Ciudad": "Madrid"},
+    {"Nombre": "Ana", "Edad": 30, "Ciudad": "Barcelona"},
+    {"Nombre": "Luis", "Edad": 35, "Ciudad": "Valencia"}
+]
+#Diccionarios a guardar
+
+# Guardar los diccionarios en un archivo csv
+leer_diccionarios_de_csv(archivo, datos)
+
+# Actividad hacer un programa llamado LeerconLibreria.py que importe la función leer_diccionarios_de_csv y lea el archivo datos.csv
